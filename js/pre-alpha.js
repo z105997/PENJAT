edbbd9cfebedf0b92469ac5d7e285c744504459f
z105997 <ncarvajalq@iesbendinat.org>
2025-02-03 14:54:55 +0100

    Add files via upload

--- js/pre-alpha.js
+++ js/pre-alpha.js
@@ -5,28 +5,8 @@
             var vidas = 7;
             var palabra ="";
             var fallo ="";
-            var seconds = 0;
-		function timer() {
-                    seconds = seconds + 1;
-                    document.getElementById("counter").innerHTML = seconds;
-		}
-		setInterval(timer,1000);
-            function cargar() {
-                document.getElementById("a0").hidden = true;
-                document.getElementById("a1").hidden = true;
-                document.getElementById("a2").hidden = true;
-                document.getElementById("a3").hidden = true;
-                document.getElementById("a4").hidden = true;
-                document.getElementById("a5").hidden = true;
-                document.getElementById("a6").hidden = true;
-                document.getElementById("a").hidden = true;
-                if (!confirm("Anam a la quinta forca?")) {
-                    document.body.style.backgroundImage = "url('img/fondo1.png')";
-            }
-        }
             function comprovador() {
                 lletra = document.getElementById("l").value;
-                document.getElementById("l").value=""; 
                 lletra = lletra.toLowerCase();
                 switch(lletra) {
                     case "á":
@@ -59,7 +39,6 @@
                   vidas = vidas - 1;
                   document.getElementById("fallo").innerHTML = 
                         fallo = fallo + lletra + " ";
-                        mostrarimg();
                 } else if (lletra === "") {
                     window.alert("Por favor introduce un caracter");
                 } else {
@@ -71,7 +50,6 @@
                 } else {
                   if (palabra.length >= 14) {
                       window.alert("Has ganado");
-                      document.getElementById("a").hidden = false;
                       parartodo();
                   } 
               }
@@ -82,34 +60,4 @@
                 document.getElementById("l").disabled = true;
                 document.getElementById("comprovar").disabled = true;
             }
-            function mostrarimg () {
-            switch (vidas) {
-                case 6:
-                   document.getElementById("a6").hidden = false;
-                   break;
-                case 5:
-                   document.getElementById("a6").hidden = true;
-                   document.getElementById("a5").hidden= false;
-                   break;
-                case 4:
-                   document.getElementById("a5").hidden = true;
-                   document.getElementById("a4").hidden= false;
-                   break;
-                case 3:
-                   document.getElementById("a4").hidden = true;
-                   document.getElementById("a3").hidden= false;
-                   break;
-                case 2:
-                   document.getElementById("a3").hidden = true;
-                   document.getElementById("a2").hidden= false;
-                   break;
-                case 1:
-                   document.getElementById("a2").hidden = true;
-                   document.getElementById("a1").hidden= false;
-                   break;
-                case 0:
-                   document.getElementById("a1").hidden = true;
-                   document.getElementById("a0").hidden= false;
-                   break; 
-            }
-            }
+

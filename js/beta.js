709a278c9785bcf0162d10e28efb523a97d4e93d
z105997 <ncarvajalq@iesbendinat.org>
2025-02-24 14:58:20 +0100

    Add files via upload

--- js/beta.js
+++ js/beta.js
@@ -4,15 +4,18 @@
  */
         //define las variables globales
             var vidas = 7;
-            var palabra =[];
-            var fallo ="";
-            var paraulesca =["cordes","fetge","forca","jutges","jutjat","mengen","quinta","setze"];
+            var palabra = [];
+            var fallo = ["_","_","_","_","_","_","_"];
+            var paraulesca =["cordes","fetge","forca","jutges","jutjat","mengen","quinta","setze","penjat"];
             var pistesca = ["A la quinta forca","Setze jutges d'un jutjat mengen fetge d'un penjat","A ca un penjat, no hi anomenis cordes"];
-            var paraulapista = [1, 2, 0, 2, 2, 2, 1, 0, 2];
+            var paraulapista = [2, 1, 0, 1, 1, 1, 0, 1, 2];
             var aleatori = Math.floor (Math.random() * paraulesca.length);
             var paraula = paraulesca[aleatori];
             var pista = pistesca[paraulapista[aleatori]];
-            
+        //Define la medida de la palabra
+                for (var i = 0; i < paraula.length; i++) {
+                  palabra[i] = "_";
+                }
         //crea un contador
             var seconds = 0;
 		function timer() {
@@ -20,7 +23,6 @@
                     document.getElementById("counter").innerHTML = seconds;
 		}
 		setInterval(timer,1000);
-                
         //oculta las imagenes al cargar la pagina
             function cargar() {
                 document.getElementById("a0").hidden = true;
@@ -34,18 +36,13 @@
                 document.getElementById("moixr").hidden = true;
                 document.getElementById("moix").hidden = true;
                 document.getElementById("moixc").hidden = true;
+                document.getElementById("palabra").innerHTML = palabra;
             //pregunta para saber que fondo poner
                 if (!confirm("Anam a la quinta forca?")) {
                     document.body.style.backgroundImage = "url('img/fondo1.png')";
                 document.body.style.display = "block";
             }
         }
-        
-        //Define la medida de la palabra
-                for (var i = 0; i < paraula.length; i++) {
-                  palabra[i] = "_";
-                }
-                
         //limpia las variantes que puedan tener las letras
             function limpiador() {
                 lletra = document.getElementById("l").value;
@@ -73,11 +70,9 @@
                         lletra = "u";
                         break;
                 }
-                
         //comprueva si la letra pertenece a la palabra
                 if (paraula.includes(lletra)) {
                   window.alert("has encertat");
-                  document.getElementById("palabra").innerHTML = 
             //reproduce sonidos de ganar
                     document.getElementById("miau").play();
                     document.getElementById("clock").play();
@@ -91,16 +86,10 @@
                   document.getElementById("fallo").innerHTML = 
                         fallo = fallo + lletra + " ";
                         mostrarimg();
-            //en caso de que no introduzcas nada
-                } else if (lletra === "") {
-                    window.alert("Por favor introduce un caracter");
-                    document.getElementById("clock").play();
-            //en caso de que no introduzcas una letra
                 } else {
                     window.alert("Caracter Incorrecto");
                     document.getElementById("clock").play();
                 }
-                
         //termina el juego segun si gans o pierdes
                 if (vidas <= 0) {
                   window.alert("Has perido");
@@ -119,25 +108,22 @@
             //para el programa y cambia el fondo
                       parartodo();
                       document.body.style.backgroundImage = "url('img/Party.png')";
-            //reproduce sonidos de victoria y osculta y muestra imagenes
+            //reproduce sonidos de victoria, osculta y muestra imagenes
                       document.getElementById("cheer").play();
                       document.getElementById("imagenes").hidden = true;
                       document.getElementById("flux").hidden = false;
                       document.getElementById("moix").hidden = true;
                       document.getElementById("moixc").hidden = false;
                             } 
-                   
        //resta las vidas
                 document.getElementById("vidas").innerHTML =
                         "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;" + vidas;
             }
-            
         //para todo el programa
             function parartodo() {
                 document.getElementById("l").disabled = true;
                 document.getElementById("comprovar").disabled = true;
             }
-            
         //muestra las imagenes segun las vidas que queden
             function mostrarimg () {
             switch (vidas) {
@@ -170,7 +156,6 @@
                    break; 
             }
         }
-        
         //muestra una pista
             function pistas() {
                 window.alert(pista);
